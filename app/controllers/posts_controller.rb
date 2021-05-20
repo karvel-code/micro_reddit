89dee00a71f7299ed24929b4de23d13a8819a912
karvel-code <velgithinji@gmail.com>
2021-05-20 22:08:16 +0300

    Add actions in post controller

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,23 @@
+class PostsController < ApplicationController
+    before_action :authenticate_user!, only: [:new, :create]
+    def new
+        @post = Post.new
+    end
+
+    def create
+        @post = current_user.posts.build(post_params)
+        if @post.save
+            redirect_to root_path
+        else
+            render :new
+        end
+    end
+
+    def index
+    end
+
+    private
+    def post_params 
+        params.require(:post).permit(:title, :body) 
+    end
+end

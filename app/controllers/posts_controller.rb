508725c4af309151a2a3fc13e3b37cebe0521808
karvel-code <velgithinji@gmail.com>
2021-05-21 11:18:55 +0300

    Correct rubocop errors

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,20 +1,22 @@
+# frozen_string_literal: true
+
 class PostsController < ApplicationController
-    include PostsHelper
-    before_action :authenticate_user!, only: [:new, :create]
-    def new
-        @post = Post.new
-    end
+  include PostsHelper
+  before_action :authenticate_user!, only: %i[new create]
+  def new
+    @post = Post.new
+  end
 
-    def create
-        @post = current_user.posts.build(post_params)
-        if @post.save
-            redirect_to root_path
-        else
-            render :new
-        end
+  def create
+    @post = current_user.posts.build(post_params)
+    if @post.save
+      redirect_to root_path
+    else
+      render :new
     end
+  end
 
-    def index
-        @posts = Post.all
-    end
+  def index
+    @posts = Post.all
+  end
 end
--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 module PostsHelper
-    def post_params 
-        params.require(:post).permit(:title, :body) 
-    end
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
 end
